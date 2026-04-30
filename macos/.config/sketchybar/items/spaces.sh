ad5078e600359bee517aef0245a04bc8571dc8c6
Aleksandr-Mazhul <map07102007@gmail.com>
2026-04-30 17:51:28 +0300

    fix(sketchybar): restore workspace app icons

--- macos/.config/sketchybar/items/spaces.sh
+++ macos/.config/sketchybar/items/spaces.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-SPACE_SIDS=($(yabai -m query --spaces | jq -r '.[] | select(."has-focus" or (.windows | length) > 0) | .index'))
-
-for sid in "${SPACE_SIDS[@]}"
+for sid in $(yabai -m query --spaces | jq -r '.[].index')
 do
   label="$(yabai -m query --spaces | jq -r ".[] | select(.index==$sid) | .label")"
   label="${label#_}"
 
   sketchybar --add space space.$sid left \
-             --set space.$sid space=$sid \
-                              icon="$label" \
-                              label.font="sketchybar-app-font:Regular:16.0" \
-                              label.padding_right=20 \
-                              label.y_offset=-1 \
-                              script="$PLUGIN_DIR/space.sh"
+             --set space.$sid \
+                  space=$sid \
+                  icon="$label" \
+                  label="" \
+                  label.font="sketchybar-app-font:Regular:16.0" \
+                  label.padding_right=20 \
+                  label.y_offset=-1 \
+                  script="$PLUGIN_DIR/space.sh"
 done
 
-sketchybar --add item space_separator left \
-           --set space_separator icon="􀆊" \
-                                 icon.color=$ACCENT_COLOR \
-                                 icon.padding_left=4 \
-                                 label.drawing=off \
-                                 background.drawing=off \
-                                 script="$PLUGIN_DIR/space_windows.sh" \
-           --subscribe space_separator space_windows_change
+# hidden worker item for events
+sketchybar --add item space_windows_worker left \
+           --set space_windows_worker drawing=off \
+                                     script="$PLUGIN_DIR/space_windows.sh" \
+           --subscribe space_windows_worker space_windows_change
