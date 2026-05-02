a42618c6729518194990b768660a6fa8195df3b3
Aleksandr-Mazhul <map07102007@gmail.com>
2026-05-03 01:07:46 +0300

    chore(env): configure python host path for neovim

--- common/.config/nvim/lua/config/options.lua
+++ common/.config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.env.PATH = '/opt/homebrew/opt/python@3.13/libexec/bin:' .. vim.env.PATH
+vim.g.python3_host_prog = '/opt/homebrew/opt/python@3.13/libexec/bin/python3'
--- common/.zsh/exports.zsh
+++ common/.zsh/exports.zsh
@@ -1,3 +1,4 @@
+export PATH="/opt/homebrew/opt/python@3.13/libexec/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
 export EDITOR="nvim"
